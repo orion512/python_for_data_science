07b33a49d5a2ed0c93286d62013dd5260422b574
Dominik Zulovec Sajovic <dominik.zsajovic@gmail.com>
2022-04-27 08:52:33 +0100

    Run the big example 2 experiment. Started writing example 3 which filters sub strings from strings.

--- list_comprehensions/example_3.py
+++ list_comprehensions/example_3.py
@@ -5,3 +5,26 @@ This module ...
 Author: Dominik Zulovec Sajovic, April 2022
 """
 
+import random
+import string
+
+def gen_rand_string(len: int):
+    """ Generaates a random string of the specified length """
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(len))
+
+
+def contains_str(s: str):
+    return True if SUB_STR in s else False
+
+
+rand_strings = [gen_rand_string(7) for i in range(10000)]
+
+SUB_STR = 'dom'
+
+mathced_strings = []
+for s in rand_strings:
+    if SUB_STR in s:
+        mathced_strings.append(s)
+
+print(mathced_strings)
